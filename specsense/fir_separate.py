5eb4c887f0e8b761342dd422634edc00c7d45800
ali-rasteh <ali.rasteh1@gmail.com>
2025-11-02 15:13:52 -0500

    debugs

--- specsense/fir_separate.py
+++ specsense/fir_separate.py
@@ -1,6 +1,6 @@
 from backend import *
 from backend import be_np as np, be_scp as scipy
-from SigProc_Comm.filter_utils import Filter_Utils
+from sigcom_toolkit.filter_utils import Filter_Utils
 
 
 
--- specsense/ss_detection_unet.py
+++ specsense/ss_detection_unet.py
@@ -1,6 +1,6 @@
 from backend import *
 from backend import be_np as np, be_scp as scipy
-from SigProc_Comm.signal_utils import Signal_Utils
+from sigcom_toolkit.signal_utils import Signal_Utils
 
 
 
